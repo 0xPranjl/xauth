06c233a649ab339153b0aa6e954582a598c7b267
Pranjal maurya <pranjal.maurya402@gmail.com>
2022-10-11 19:30:22 +0530

    added login function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Web3=require("web3");
 const abi=require("./abi.json");
 const web3 = new Web3('https://api.s0.ps.hmny.io');
+const axios=require("axios");
 const contract_address="0xdC092Ea4D6e83B303ff731ACdC6f4Bf3764Fb803";
 const contract = new web3.eth.Contract(abi, contract_address);
 async function nodecount(){
@@ -44,4 +45,27 @@ async function allnoderpc(){
    }
 }
 
+async function login(){
+  var no=await nodecount();
+  let rpcurl=[];
+  let jwtsign=[];
+  let whoissign=[];
+  for(var x=0;x<no;x++){
+    var rpc=await noderpc(x);
+    rpcurl.push(rpc);
+  }
+  axios.all(rpcurl.map((endpoint) => axios.get(endpoint))).then(
+    (data) =>{
+       //console.log(data[0]);
+      var fullres=data.split("$");
+      jwtsign.push(fullres[0]);
+      whoissign.push(whoissign[0]);
+    }
+  );
+  
+  
+ 
+  
+}
+login();
 module.exports={nodecount,nodenameat,noderpc,allnoderpc}
